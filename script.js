a2939eac310b97e85360c9c15ffbf0fc6a3698c6
Cbhaskar143 <125853438+Cbhaskar143@users.noreply.github.com>
2023-02-26 15:42:45 +0530

    bulb
    style.css,index.html,script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function show1(){
+    document.getElementById("image").setAttribute("src","images/1.webp")
+    }
+function show2(){
+    document.getElementById("image").setAttribute("src","images/2.webp")
+    }
+function show3(){
+    document.getElementById("image").setAttribute("src","images/4.webp")
+    }
+function show4(){
+    document.getElementById("image").setAttribute("src","images/5.webp")
+    }
+function show5(){
+    document.getElementById("image").setAttribute("src","images/6.webp")
+    }
+let className=document.getElementsByClassName("picture")
+console.log(className)
